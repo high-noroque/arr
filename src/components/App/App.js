ee6fe047cf83a766270ec0748c095c22f2d0c44e
positronmihai@gmail.com <serejanxxx@gmail.com>
2018-10-29 17:35:07 +0300

    basic project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import CommentList from '../CommentList/CommentList'
+import CommentBox from '../CommentBox/CommentBox'
+
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  )
+}
